89ae6c5a13fc5abcbe0918ce294006820ee9f897
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-11-19 20:47:55 +0900

    Add top view

--- djangosnippets/snippets/tests.py
+++ djangosnippets/snippets/tests.py
@@ -1,3 +1,15 @@
+from django.http import HttpRequest
 from django.test import TestCase
+from snippets.views import top
 
-# Create your tests here.
+
+class TopPageViewTestCase(TestCase):
+    def test_top_returns_200(self):
+        request = HttpRequest()
+        response = top(request)
+        self.assertEqual(response.status_code, 200)
+
+    def test_top_returns_expected_content(self):
+        request = HttpRequest()
+        response = top(request)
+        self.assertEqual(response.content, b'Hello World')
