888a4c375ab630058610ecf4a77321b01b0ae991
toshiaki-suzuki <saki0x4bf@gmail.com>
2023-12-01 08:17:27 +0900

    Add snippet edit

--- djangosnippets/snippets/views.py
+++ djangosnippets/snippets/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.decorators import login_required
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseForbidden
 from django.shortcuts import render, redirect, get_object_or_404
 from snippets.models import Snippet
 from snippets.forms import SnippetForm
@@ -25,8 +25,19 @@ def snippet_new(request):
     return render(request, "snippets/snippet_new.html", {'form': form})
 
 
+@login_required
 def snippet_edit(request, snippet_id):
-    return HttpResponse('スニペットの編集')
+    snippet = get_object_or_404(Snippet, pk=snippet_id)
+    if snippet.created_by != request.user:
+        return HttpResponseForbidden('このスニペットの編集は許可されていません。')
+
+    if request.method == 'POST':
+        form = SnippetForm(request.POST, instance=snippet)
+        if form.is_valid():
+            snippet = form.save()
+            return redirect(snippet_detail, snippet_id=snippet_id)
+    else:
+        form = SnippetForm(instance=snippet)  # instanceのスニペットの値がデフォルト値を設定
 
 
 def snippet_detail(request, snippet_id):
